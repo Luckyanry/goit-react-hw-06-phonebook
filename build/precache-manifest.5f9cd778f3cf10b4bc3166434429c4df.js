daac8698737eda9a106ef7bb1ca5eaca7867ebac
Andrii Kochmaruk <luckyanry@gmail.com>
2020-08-05 00:06:00 +0200

    commit after deploy

---
+++ build/precache-manifest.5f9cd778f3cf10b4bc3166434429c4df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f71aa654ac3346ccd2a22fe55592d1",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "b43b79014a816b06e4a4",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.53d2d973.chunk.css"
+  },
+  {
+    "revision": "c74b253029d4f7eb262c",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.3c822421.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.3c822421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43b79014a816b06e4a4",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.06431915.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
